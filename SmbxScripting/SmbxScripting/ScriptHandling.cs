59f52072a362a83b70f4d7253a5fe9175486e785
Mike Santiago <miketheripper1@gmail.com>
2014-07-07 07:54:32 -0400

    Initial Commit

---
+++ SmbxScripting/SmbxScripting/ScriptHandling.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmbxScripting
+{
+    public class ScriptHandling
+    {
+        /// <summary>
+        /// Displays a message prompt for the user
+        /// </summary>
+        /// <param name="message">The message to be shown</param>
+        /// <param name="type">The type of message to show
+        /// 0=OK Only Message
+        /// 1=Yes or No message</param>
+        public void userPrompt(string message, int type)
+        {
+            if (type == 0)
+            {
+                MessageBox.Show(message, "Script", MessageBoxButtons.OK);
+            }
+            else if (type == 1)
+            {
+                MessageBox.Show(message, "Script", MessageBoxButtons.YesNo);
+            }
+        }
+
+        public void copyFile(string source, string dest)
+        {
+
+        }
+    }
+}
